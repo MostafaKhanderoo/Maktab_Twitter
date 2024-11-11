9339b4f45666cad4b3ae700eb0d5ef8afb024215
mostafa <mostafakhanderoo7@gmail.com>
2024-11-11 11:09:02 +0330

    Add fields for Tweet

--- src/main/java/entity/Tweet.java
+++ src/main/java/entity/Tweet.java
@@ -1,4 +1,17 @@
 package entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.lang.reflect.Array;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Tweet {
+    private int id;
+    private String content;
+    private String retweet;
+
 }
