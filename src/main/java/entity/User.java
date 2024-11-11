46e54af8e9f913bd9c8c1c6019048d1b797776bb
mostafa <mostafakhanderoo7@gmail.com>
2024-11-11 10:53:42 +0330

    add 7 fields for User

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,4 +1,22 @@
 package entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Calendar;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    private int userId;
+    private String accountName;
+    private String email;
+    private String username;
+    private String password;
+    private String bio;
+    private Calendar createDate;
+
+
 }
