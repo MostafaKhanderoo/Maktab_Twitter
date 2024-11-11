09b3a9feb30fa234103803279029329483cfabe9
mostafa <mostafakhanderoo7@gmail.com>
2024-11-11 20:39:46 +0330

    test user method

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -1,9 +1,17 @@
 package view;
 
+import entity.User;
+import repository.UserRepository;
+
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        UserRepository userRepository = new UserRepository();
+        User user = new User(1,"mostafa", "mostafakhanderoo7@gmail.com","mostafa","11684","hello my name is mostafa",null,1);
 
+
+
+   userRepository.deleteById(1);
     }
 }
