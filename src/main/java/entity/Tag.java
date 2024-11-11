8bb58fb99603944922d8901dad9565401c92c45e
mostafa <mostafakhanderoo7@gmail.com>
2024-11-11 11:02:57 +0330

    Add fields for tag

--- src/main/java/entity/Tag.java
+++ src/main/java/entity/Tag.java
@@ -1,4 +1,13 @@
 package entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Tag {
+    private int id;
+    private String title;
 }
