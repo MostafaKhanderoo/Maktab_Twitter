ef448877156f44f840bcbbecb2ba8d965134bfe7
mostafa <mostafakhanderoo7@gmail.com>
2024-11-11 11:40:17 +0330

    Datasource for connection with postgres SQL

---
+++ src/main/java/datasource/Datasource.java
@@ -0,0 +1,37 @@
+package datasource;
+
+import lombok.Getter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Datasource {
+    @Getter
+    private static final Connection connection;
+    private Datasource(){
+
+    }
+    static {
+        var jdbc="jdbc:postgresql://localhost:5432/postgres";
+        try {
+             connection = DriverManager.getConnection(jdbc,"postgres","mostafa11684");
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            public void run(){
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                   throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+
+   public static Connection geConnection(){
+    return connection;
+   }
+
+}
