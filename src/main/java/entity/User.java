ce36f1b6c0d693ba421ccd0671a698721c12ecc8
mostafa <mostafakhanderoo7@gmail.com>
2024-11-11 11:09:30 +0330

    Add Lombok add lombok dependency to User

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.Calendar;
 
 @Data
@@ -16,7 +17,7 @@ public class User {
     private String username;
     private String password;
     private String bio;
-    private Calendar createDate;
+    private LocalDate createDate;
 
 
 }
