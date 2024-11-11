9242509e5ea1779f41ad0d4b86d02b26cf197a68
mostafa <mostafakhanderoo7@gmail.com>
2024-11-11 23:48:41 +0330

    add field date for tweet time created

--- src/main/java/entity/Tweet.java
+++ src/main/java/entity/Tweet.java
@@ -3,6 +3,8 @@ package entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -11,6 +13,7 @@ import java.util.List;
 public class Tweet {
     private long tweetId;
     private String content;
+    private Date createDate;
     private List<Tag> tags;
     private long userId;
 
