b95b36cb596ff6e4fafe23eca4bc38728b17e3be
mostafa <mostafakhanderoo7@gmail.com>
2024-11-12 10:38:51 +0330

    add method save tweet class TweetRepository

--- src/main/java/repository/TweetRepository.java
+++ src/main/java/repository/TweetRepository.java
@@ -1,24 +1,39 @@
 package repository;
 
+import datasource.Datasource;
+import entity.Tweet;
+
+import java.sql.SQLException;
+
 public class TweetRepository {
     private static final  String INSERT_SQL  = """
-            INSERT INTO USERS()
-            VALUES(?,?,?,?,?)
+            INSERT INTO tweet()
+            VALUES(?)
             """;
     private static final String DELETE_BY_ID_SQL= """
-            DELETE FROM USERS 
+            DELETE FROM tweet 
             WHERE id= ?
             """;
     private static final String FIND_BY_ID_SQL = """
-            SELECT * FROM USERS 
+            SELECT * FROM tweet 
             WHERE ID  = ?
             """;
-    private static final String FIND_BY_USERNAME = """
-            SELECT * FROM USERS
-            WHERE username = ?
+    private static final String FIND_BY_CREATOR = """
+            SELECT * FROM tweet
+            WHERE tweet = ?
             """;
-    private static final String FIND_BY_EMAIL= """
-            SELECT * FROM USERS
-            WHERE email=?
-            """;
-}
+
+    public Tweet save (Tweet tweet)throws SQLException{
+      var statement =  Datasource.getConnection().prepareStatement(INSERT_SQL);
+            statement.setString(1,tweet.getContent());
+            statement.execute();
+            statement.close();
+            return tweet;
+
+        }
+    }
+
+
+
+
+
