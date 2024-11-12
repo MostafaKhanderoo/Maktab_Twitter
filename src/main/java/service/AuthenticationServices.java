0ef83c276ebbdc758c80afede8d0f2b2e901d1a2
mostafa <mostafakhanderoo7@gmail.com>
2024-11-12 15:18:04 +0330

    create AuthenticationServices for log User

---
+++ src/main/java/service/AuthenticationServices.java
@@ -0,0 +1,17 @@
+package service;
+
+import entity.User;
+
+public class AuthenticationServices {
+    private static User loggedInUser;
+
+    public static void setLoggedUser(User user){
+        loggedInUser = user;
+    }
+    public static User getLoggedInUser (){
+        return loggedInUser;
+    }
+    public static void logout(){
+        loggedInUser = null;
+    }
+}
