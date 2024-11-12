5d575a897730c44ddbad3db57340f2d21f200c08
mostafa <mostafakhanderoo7@gmail.com>
2024-11-12 13:02:22 +0330

    Add method show all tags TagService

--- src/main/java/service/TagService.java
+++ src/main/java/service/TagService.java
@@ -1,4 +1,15 @@
 package service;
 
-public class TagService {
+import entity.Tag;
+import repository.TagRepository;
+
+import java.util.List;
+
+public class TagService  {
+    TagRepository tagRepository;
+    public List<Tag> viewAllTags(){
+    List<Tag> tags = tagRepository.findAllTags() ;
+        return tags;
+    }
+
 }
