c725c0a7fd908632e9d38aadfb83c6dd16ce307b
mostafa <mostafakhanderoo7@gmail.com>
2024-11-12 11:31:03 +0330

    update login with username and password

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -15,29 +15,41 @@ public class UserService {
       tweetRepository = new TweetRepository();
   }
 
-    public User userSignUp(String username, String password) throws SQLException {
+    public User userSignUp(String accountName,String email,String username, String password,String bio) throws SQLException {
         var checkingUser = userRepository.findByUsername(username);
-
+         var checkingEmail  = userRepository.findByEmail(email);
+        User user = new User();
         if (checkingUser != null) {
             System.out.println("Username is already taken!");
             return null;
+        }else
+        if (checkingEmail != null){
+            System.out.println("email is already taken!");
+            return null;
+        }else {
+
+            User signing = new User();
+            signing.setAccountName(accountName);
+            signing.setEmail(email);
+            signing.setUsername(username);
+            signing.setPassword(password);
+            signing.setBio(bio);
+
+            return userRepository.save(signing);
         }
 
-        User signingUpUser = new User();
-        signingUpUser.setUsername(username);
-        signingUpUser.setPassword(password);
-
-        return userRepository.save(signingUpUser);
     }
 
     public boolean userLogin(String username, String password) throws SQLException {
-        var checkingUser = userRepository.findByUsername(username);
-        if (checkingUser != null) {
-            if (checkingUser.getPassword().equals(password)) {
-                AuthenticationServices.setLoggedUser(checkingUser);
+        var checkingUserName = userRepository.findByUsername(username);
+        var checkingUserEmail = userRepository.findByUsername(username);
+        if (checkingUserName != null || checkingUserEmail !=null) {
+            if (checkingUserName.getPassword().equals(password) || checkingUserEmail.getPassword().equals(password)) {
+                AuthenticationServices.setLoggedUser(checkingUserName);
                 return true;
             }
-        }
+        }else
+            System.out.println("UserName or email not found try again!  ");
         return false;
     }
 }
