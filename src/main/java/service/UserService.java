890d9f077c1869258a6ef64794f96e86ef92ca32
mostafa <mostafakhanderoo7@gmail.com>
2024-11-12 09:49:41 +0330

    add log and signup in UserService for User

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,5 +1,43 @@
 package service;
 
-public class UserService {
+import entity.Tweet;
+import entity.User;
+import repository.TweetRepository;
+import repository.UserRepository;
 
+import java.sql.SQLException;
+
+public class UserService {
+    UserRepository userRepository;
+    TweetRepository tweetRepository;
+  public UserService() throws SQLException {
+      userRepository = new UserRepository();
+      tweetRepository = new TweetRepository();
+  }
+
+    public User userSignUp(String username, String password) throws SQLException {
+        var checkingUser = userRepository.findByUsername(username);
+
+        if (checkingUser != null) {
+            System.out.println("Username is already taken!");
+            return null;
+        }
+
+        User signingUpUser = new User();
+        signingUpUser.setUsername(username);
+        signingUpUser.setPassword(password);
+
+        return userRepository.save(signingUpUser);
+    }
+
+    public boolean userLogin(String username, String password) throws SQLException {
+        var checkingUser = userRepository.findByUsername(username);
+        if (checkingUser != null) {
+            if (checkingUser.getPassword().equals(password)) {
+                AuthenticationServices.setLoggedUser(checkingUser);
+                return true;
+            }
+        }
+        return false;
+    }
 }
