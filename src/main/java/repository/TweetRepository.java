07f91c5169a1dbb81db7986f77b426225e57327f
mostafa <mostafakhanderoo7@gmail.com>
2024-11-12 10:31:22 +0330

    Add CRUD

--- src/main/java/repository/TweetRepository.java
+++ src/main/java/repository/TweetRepository.java
@@ -1,4 +1,24 @@
 package repository;
 
 public class TweetRepository {
+    private static final  String INSERT_SQL  = """
+            INSERT INTO USERS()
+            VALUES(?,?,?,?,?)
+            """;
+    private static final String DELETE_BY_ID_SQL= """
+            DELETE FROM USERS 
+            WHERE id= ?
+            """;
+    private static final String FIND_BY_ID_SQL = """
+            SELECT * FROM USERS 
+            WHERE ID  = ?
+            """;
+    private static final String FIND_BY_USERNAME = """
+            SELECT * FROM USERS
+            WHERE username = ?
+            """;
+    private static final String FIND_BY_EMAIL= """
+            SELECT * FROM USERS
+            WHERE email=?
+            """;
 }
