8d17d102b75b6317892ac6058ac234afeaab7127
mostafa <mostafakhanderoo7@gmail.com>
2024-11-12 17:38:00 +0330

    update find all tweet TweetService

--- src/main/java/service/TweetService.java
+++ src/main/java/service/TweetService.java
@@ -1,4 +1,23 @@
 package service;
 
+import entity.Tag;
+import repository.TagRepository;
+import repository.TweetRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class TweetService {
+
+TweetRepository tweetRepository;
+public  void createNewTweet(String content) throws SQLException {
+    tweetRepository.save(content);
+}
+
+    TagRepository tagRepository;
+    public List<Tag> viewAllTags(){
+        List<Tag> tags = tagRepository.findAllTags() ;
+        return tags;
+    }
+
 }
