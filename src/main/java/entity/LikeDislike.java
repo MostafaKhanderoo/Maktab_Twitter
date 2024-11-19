b309fca644b8064cf0f3990638961cc44d18dc21
mostafa <mostafakhanderoo7@gmail.com>
2024-11-19 13:31:04 +0330

    create class foe like and dislike tweets

---
+++ src/main/java/entity/LikeDislike.java
@@ -0,0 +1,7 @@
+package entity;
+
+public enum LikeDislike {
+
+    LIKE,
+    DIS_LIKE
+}
---
+++ src/main/java/entity/LikeTweet.java
@@ -0,0 +1,8 @@
+package entity;
+
+public class LikeTweet {
+
+    private long userId;
+    private long tweetId;
+    private LikeDislike likeDislike;
+}
